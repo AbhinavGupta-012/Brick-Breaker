cd408d2259c1882fccd88ff07aef09195f9cc5da
Vasu3198 <vasu2398.be23@chitkara.edu.in>
2026-04-18 22:11:20 +0530

    Created starting objects

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Brick Breaker");
+
+        GamePanel panel = new GamePanel();
+
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
